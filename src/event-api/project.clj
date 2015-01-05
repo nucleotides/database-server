9981a01a8d04e85d6dae8f49bd588231b2407a93
Michael Barton <mail@michaelbarton.me.uk>
2015-01-05 14:43:13 -0800

    Remove license
    Signed-off-by: Michael Barton <mail@michaelbarton.me.uk>

--- src/event-api/project.clj
+++ src/event-api/project.clj
@@ -1,8 +1,5 @@
 (defproject event-api "current"
   :description "FIXME: write description"
-  :url "http://example.com/FIXME"
-  :license {:name "Eclipse Public License"
-            :url "http://www.eclipse.org/legal/epl-v10.html"}
 
   :dependencies [[org.clojure/clojure     "1.6.0"]
                  [ring/ring-jetty-adapter "1.3.1"]
