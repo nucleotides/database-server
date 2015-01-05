fc38a78665a42ddd3b8b054161b370ac6545c382
Michael Barton <mail@michaelbarton.me.uk>
2015-01-05 15:31:12 -0800

    Remove jetty requirements
    Signed-off-by: Michael Barton <mail@michaelbarton.me.uk>

--- src/event-api/src/event_api/core.clj
+++ src/event-api/src/event_api/core.clj
@@ -1,6 +1,5 @@
 (ns event-api.core
-  (:require [ring.adapter.jetty :as jetty]
-            [compojure.core :refer [defroutes context GET POST]]))
+  (:require [compojure.core :refer [GET defroutes]]))
 
 (defroutes events
   (GET "/events" request str))
