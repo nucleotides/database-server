ef7dc0a30c2615b9e1758ff51ca5077d219507a8
Michael Barton <mail@michaelbarton.me.uk>
2015-01-05 14:42:19 -0800

    Add test skeleton
    Signed-off-by: Michael Barton <mail@michaelbarton.me.uk>

--- src/event-api/project.clj
+++ src/event-api/project.clj
@@ -3,10 +3,14 @@
   :url "http://example.com/FIXME"
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
+
   :dependencies [[org.clojure/clojure     "1.6.0"]
                  [ring/ring-jetty-adapter "1.3.1"]
                  [compojure               "1.3.1"]]
   :plugins      [[lein-ring "0.8.13"]]
+
   :ring     {:handler event-api.core/events}
-  :profiles {:dev     {}
-             :uberjar {:aot :all}})
+
+  :profiles {
+    :dev     {:dependencies [[ring-mock "0.1.5"]]}
+    :uberjar {:aot :all}})
---
+++ src/event-api/test/event_api/core_test.clj
@@ -0,0 +1,6 @@
+(ns event-api.core-test
+  (:require [clojure.test   :refer :all]
+            [ring.mock.request :as mock]
+            [event-api.core    :as event]))
+
+(deftest events)
