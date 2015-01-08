1db750385b8c01465bd556cf75f67f64e2d25691
Michael Barton <mail@michaelbarton.me.uk>
2015-01-08 14:22:25 -0800

    Return 202 for correct post requests
    Signed-off-by: Michael Barton <mail@michaelbarton.me.uk>

--- src/event_api/core.clj
+++ src/event_api/core.clj
@@ -1,12 +1,23 @@
 (ns event-api.core
-  (:require [compojure.core :refer [GET POST defroutes]]))
+  (:require [compojure.core         :refer [GET POST defroutes]]
+            [clojure.set            :refer [superset?]]
+            [ring.middleware.params :refer [wrap-params]]))
 
 (defn post-event
   "Process a post event request. Return 202 if
   valid otherwise return appropriate HTTP error
   code otherwise."
   [request]
-  {:status 422})
+  (let [required-params #{"benchmark_id"
+                          "benchmark_type_code"
+                          "status_code"
+                          "event_type_code"}
+        request-params  (set (keys (:params request)))]
+    (if (superset? request-params required-params)
+      {:status 202}
+      {:status 422})))
 
-(defroutes api
-  (POST "/events" request post-event))
+(defroutes routes
+  (POST "/events" [] post-event))
+
+(def api (wrap-params routes))
--- test/event_api/core_test.clj
+++ test/event_api/core_test.clj
@@ -6,9 +6,19 @@
 (defn request [method url params]
   (event-api.core/api (ring.mock.request/request method url params)))
 
-(defn test-request [r status-code]
+(defn test-req [r status-code]
   (let [response (apply request r)]
     (is (= (:status response) status-code))))
 
-(deftest events
-  (test-request [:post "/events" {}] 422))
+(deftest post-events
+
+  (testing "post request with missing parameters"
+    (let [f (fn [params] (test-req [:post "/events" params] 422))]
+      (f {})
+      (f {:benchmark_id "abcd"})
+      (f {:benchmark_id "abcd" :benchmark_type_code "000" :event_type_code "000"})))
+
+  (testing "post request with valid parameters"
+    (let [f (fn [params] (test-req [:post "/events" params] 202))]
+      (f {:benchmark_id "abcd" :benchmark_type_code "0000"
+          :status_code  "0000" :event_type_code     "0000"}))))
