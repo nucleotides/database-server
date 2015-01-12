40a43df4ba53fbb67368d1ed584de0885ccb20c0
Michael Barton <mail@michaelbarton.me.uk>
2015-01-11 20:26:19 -0800

    Update lein-ring plugin
    Signed-off-by: Michael Barton <mail@michaelbarton.me.uk>

--- project.clj
+++ project.clj
@@ -6,10 +6,12 @@
                  [compojure                  "1.3.1"]
                  [com.cemerick/rummage       "1.0.1"]
                  [com.amazonaws/aws-java-sdk "1.3.21.1"]]
-  :plugins      [[lein-ring "0.8.13"]]
+  :plugins      [[lein-ring "0.9.0"]]
+
 
   :ring     {:handler event-api.core/api :port 8080}
 
   :profiles {
     :dev     {:dependencies [[ring-mock "0.1.5"]]}
     :uberjar {:aot :all}})
+
