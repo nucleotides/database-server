4ea4e13eb1d71b23dc06ac61b0aef9dd33144eae
Michael Barton <mail@michaelbarton.me.uk>
2015-01-15 10:28:57 -0800

    Drop and recreate test database around features
    Signed-off-by: Michael Barton <mail@michaelbarton.me.uk>

---
+++ features/support/sdb.rb
@@ -0,0 +1,19 @@
+module SDB
+  require 'fog/aws/simpledb'
+
+  def self.client
+    Fog::AWS::SimpleDB.new(
+      aws_access_key_id:     ENV['AWS_ACCESS_KEY'],
+      aws_secret_access_key: ENV['AWS_SECRET_KEY'],
+      region:                "us-west-1")
+  end
+
+  def self.create
+    client.create_domain(ENV['AWS_SDB_DOMAIN'])
+  end
+
+  def self.destroy
+    client.delete_domain(ENV['AWS_SDB_DOMAIN'])
+  end
+
+end
