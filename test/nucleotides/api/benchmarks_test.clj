3fae92c11708cf02e93a739e1462be1ee525ef75
Michael Barton <mail@michaelbarton.me.uk>
2015-11-16 22:43:22 -0800

    Check empty response with `empty?` instead of `empty`

--- test/nucleotides/api/benchmarks_test.clj
+++ test/nucleotides/api/benchmarks_test.clj
@@ -33,15 +33,15 @@
                 (is (contains? (first-entry-in-response body) k))))))
 
         (testing "with product=false request parameter"
-          (let [{:keys [status body]} (show {"product" "false"})]
+          (let [{:keys [status body]} (show {:product false})]
             (is (= 200 status))
-            (is (not (empty (json/read-str body))))
+            (is (not (empty? (json/read-str body))))
             (dorun
               (for [k [:id :image_task :image_name :image_sha256
                        :input_url :input_md5 :product :evaluation]]
                 (is (contains? (first-entry-in-response body) k))))))
 
         (testing "with product=true request parameter"
-          (let [{:keys [status body]} (show {"product" "true"})]
+          (let [{:keys [status body]} (show {:product true})]
             (is (= 200 status))
-            (is (empty (json/read-str body)))))))))
+            (is (empty? (json/read-str body)))))))))
