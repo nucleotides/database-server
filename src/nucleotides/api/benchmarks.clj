f2bcf113f26f763b68837330dae08584ce7a9c08
Michael Barton <mail@michaelbarton.me.uk>
2015-11-18 10:12:12 -0800

    Fix issue where strings being passed to yesql query

--- src/nucleotides/api/benchmarks.clj
+++ src/nucleotides/api/benchmarks.clj
@@ -1,6 +1,5 @@
 (ns nucleotides.api.benchmarks
-  (:require [clojure.core.match  :refer [match]]
-            [yesql.core          :refer [defqueries]]
+  (:require [yesql.core          :refer [defqueries]]
             [clojure.walk        :as walk]
             [clojure.string      :as st]
             [clojure.data.json   :as json]
@@ -8,28 +7,24 @@
 
 (defqueries "nucleotides/api/benchmarks.sql")
 
-(defn json-response [f args db-client]
-  {:status 200
-   :body   (->> {:connection db-client}
-                (f args)
-                (json/write-str))})
-
 (defn parse-boolean [v]
   (cond
     (.equalsIgnoreCase v "true")   true
     (.equalsIgnoreCase v "false")  false
     :else v))
 
-(def parse-keys
+(def parse-values
   (comp
     (partial into {})
     (partial map (fn [[k v]] [k (parse-boolean v)]))))
 
 (defn show
-  "Returns all benchmarks, can be parameterised by completed or not."
+  "Returns all benchmarks, can be parameterised by product/evaluation completed or
+  not."
   [db-client {params :params}]
-  (let [{:keys [product evaluation]} (parse-keys params)]
-    (match [product evaluation]
-           [nil nil] (json-response benchmarks            params db-client)
-           [_   nil] (json-response benchmarks-by-product params db-client)
-           :else     (json-response benchmarks-by-eval    params db-client))))
+  {:status 200
+   :body   ((cond
+              (contains? params :evaluation) benchmarks-by-eval
+              (contains? params :product)    benchmarks-by-product
+              :else                          benchmarks)
+            (parse-values params) {:connection db-client})})
--- test/nucleotides/api/benchmarks_test.clj
+++ test/nucleotides/api/benchmarks_test.clj
@@ -8,11 +8,9 @@
             [helper                           :as help]))
 
 (defn show
-  ([params] (bench/show (con/create-connection) {:query-params params}))
+  ([params] (bench/show (con/create-connection) {:params params
+                                                 :query-params params}))
   ([]       (show {})))
-
-(def first-entry-in-response
-  (comp walk/keywordize-keys first json/read-str))
 
 (deftest nucleotides.api.benchmarks
 
@@ -24,22 +22,22 @@
         (testing "with no request parameter"
           (let [{:keys [status body]} (show)]
             (is (= 200 status))
-            (is (= 1 (count (json/read-str body))))
+            (is (not (empty? body)))
             (dorun
               (for [k [:id :image_task :image_name :image_sha256
                        :input_url :input_md5 :product :evaluation]]
-                (is (contains? (first-entry-in-response body) k))))))
+                (is (contains? (first body) k))))))
 
         (testing "with product=false request parameter"
-          (let [{:keys [status body]} (show {:product false})]
+          (let [{:keys [status body]} (show {:product "false"})]
             (is (= 200 status))
-            (is (not (empty? (json/read-str body))))
+            (is (not (empty? body)))
             (dorun
               (for [k [:id :image_task :image_name :image_sha256
                        :input_url :input_md5 :product :evaluation]]
-                (is (contains? (first-entry-in-response body) k))))))
+                (is (contains? (first body) k))))))
 
         (testing "with product=true request parameter"
-          (let [{:keys [status body]} (show {:product true})]
-            (is (= 200 status))
-            (is (empty? (json/read-str body)))))))))
+          (let [{:keys [status body]} (show {:product "true"})]
+            (is (= 200   status))
+            (is (empty?  body))))))))
