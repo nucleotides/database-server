1f7d2552c2ec1aa2be0ef2ef1f53b5ed87495167
Michael Barton <mail@michaelbarton.me.uk>
2015-11-17 22:50:35 -0800

    Convert param keys to keywords

--- src/nucleotides/api/core.clj
+++ src/nucleotides/api/core.clj
@@ -1,10 +1,11 @@
 (ns nucleotides.api.core
   (:gen-class)
-  (:require [compojure.core          :refer [GET POST routes]]
-            [compojure.handler       :refer [site]]
-            [ring.middleware.params  :refer [wrap-params]]
-            [ring.logger.timbre      :refer [wrap-with-logger]]
-            [ring.adapter.jetty      :refer [run-jetty]]
+  (:require [compojure.core                  :refer [GET POST routes]]
+            [compojure.handler               :refer [site]]
+            [ring.middleware.keyword-params  :refer [wrap-keyword-params]]
+            [ring.middleware.params          :refer [wrap-params]]
+            [ring.logger.timbre              :refer [wrap-with-logger]]
+            [ring.adapter.jetty              :refer [run-jetty]]
 
             [nucleotides.database.connection  :as con]
             [nucleotides.api.database         :as db]
@@ -14,6 +15,7 @@
 (defn api [database-client]
   (-> (routes (GET "/benchmarks/show.json" [] (partial benchmarks/show database-client)))
       (wrap-with-logger)
+      (wrap-keyword-params)
       (wrap-params)))
 
 (defn -main [& args]
