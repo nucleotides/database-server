0e4cdde34509f611d63bdc92c2fbc28158634172
Michael Barton <mail@michaelbarton.me.uk>
2015-11-17 22:49:57 -0800

    Parse benchmark params into clojure values

--- src/nucleotides/api/benchmarks.clj
+++ src/nucleotides/api/benchmarks.clj
@@ -1,10 +1,10 @@
 (ns nucleotides.api.benchmarks
   (:require [clojure.core.match  :refer [match]]
             [yesql.core          :refer [defqueries]]
-
             [clojure.walk        :as walk]
             [clojure.string      :as st]
-            [clojure.data.json   :as json]))
+            [clojure.data.json   :as json]
+            [taoensso.timbre     :as log]))
 
 (defqueries "nucleotides/api/benchmarks.sql")
 
@@ -14,10 +14,21 @@
                 (f args)
                 (json/write-str))})
 
+(defn parse-boolean [v]
+  (cond
+    (.equalsIgnoreCase v "true")   true
+    (.equalsIgnoreCase v "false")  false
+    :else v))
+
+(def parse-keys
+  (comp
+    (partial into {})
+    (partial map (fn [[k v]] [k (parse-boolean v)]))))
+
 (defn show
   "Returns all benchmarks, can be parameterised by completed or not."
-  [db-client {params :query-params}]
-  (let [{:keys [product evaluation]} (walk/keywordize-keys params)]
+  [db-client {params :params}]
+  (let [{:keys [product evaluation]} (parse-keys params)]
     (match [product evaluation]
            [nil nil] (json-response benchmarks            params db-client)
            [_   nil] (json-response benchmarks-by-product params db-client)
