4669dc80e69f6891d1d0195d17d4e73e69606b8b
Michael Barton <mail@michaelbarton.me.uk>
2015-11-20 10:26:20 -0800

    Convert parameters to booleans in SQL statement

--- src/nucleotides/api/benchmarks.clj
+++ src/nucleotides/api/benchmarks.clj
@@ -7,17 +7,6 @@
 
 (defqueries "nucleotides/api/benchmarks.sql")
 
-(defn parse-boolean [v]
-  (cond
-    (.equalsIgnoreCase v "true")   true
-    (.equalsIgnoreCase v "false")  false
-    :else v))
-
-(def parse-values
-  (comp
-    (partial into {})
-    (partial map (fn [[k v]] [k (parse-boolean v)]))))
-
 (defn show
   "Returns all benchmarks, can be parameterised by product/evaluation completed or
   not."
@@ -27,7 +16,7 @@
               (contains? params :evaluation) benchmarks-by-eval
               (contains? params :product)    benchmarks-by-product
               :else                          benchmarks)
-            (parse-values params) {:connection db-client})})
+            params {:connection db-client})})
 
 (defn create
   "Creates a new benchmark event from the given parameters"
--- src/nucleotides/api/benchmarks.sql
+++ src/nucleotides/api/benchmarks.sql
@@ -4,11 +4,11 @@ SELECT * FROM benchmark_instance_status;
 
 -- name: benchmarks-by-product
 -- Get all benchmark entries by product status
-SELECT * FROM benchmark_instance_status WHERE product = :product;
+SELECT * FROM benchmark_instance_status WHERE product = :product::boolean;
 
 -- name: benchmarks-by-eval
 -- Get all benchmark entries by evaluation status
-SELECT * FROM benchmark_instance_status WHERE evaluation = :evaluation;
+SELECT * FROM benchmark_instance_status WHERE evaluation = :evaluation::boolean;
 
 -- name: create-benchmark-event<!
 -- Create a new benchmark event
