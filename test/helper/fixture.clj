26922a8cf793139d8759c3bc868da0b657e24b4c
Michael Barton <mail@michaelbarton.me.uk>
2015-12-11 17:53:06 -0800

    Test helper to load multiple fixtures

--- test/helper/fixture.clj
+++ test/helper/fixture.clj
@@ -6,15 +6,11 @@
 (defn test-directory [which]
   (str "test/" (ksk/->snake_case_string which)))
 
-(defn test-directory-file [which file]
-  (-> (test-directory which)
-      (str file)
-      (clojure.java.io/file)
-      (.getCanonicalPath)))
-
 (defn fetch-test-data [x]
   (build/load-data-file (test-directory :data) x))
 
-(defn load-fixture [x]
-  (do (db/empty-database)
-      (db/exec-db-command (slurp (str "test/fixtures/" x ".sql")))))
+(defn load-fixture [& fixtures]
+  (db/empty-database)
+  (dorun
+    (for [f fixtures]
+      (db/exec-db-command (slurp (str "test/fixtures/" f ".sql"))))))
