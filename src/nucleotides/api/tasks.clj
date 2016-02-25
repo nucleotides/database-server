4a07626f803262c492c855ef78ae8343fa179af3
Michael Barton <mail@michaelbarton.me.uk>
2016-02-24 17:47:09 -0800

    Implement fetching events when looking up task

--- src/nucleotides/api/tasks.clj
+++ src/nucleotides/api/tasks.clj
@@ -1,7 +1,8 @@
 (ns nucleotides.api.tasks
-  (:require [clojure.set         :as st]
-            [yesql.core          :refer [defqueries]]
-            [ring.util.response  :as ring]))
+  (:require [clojure.set            :as st]
+            [yesql.core             :refer [defqueries]]
+            [ring.util.response     :as ring]
+            [nucleotides.api.events :as event]))
 
 (defqueries "nucleotides/api/tasks.sql")
 (defqueries "nucleotides/api/benchmarks.sql")
@@ -19,27 +20,35 @@
               (assoc kvs k))
          (keys ks)))
 
-
 (defn get-task-files [db-client benchmark-instance-id task-type]
+  "Fetch all input files for a given task"
   ((if (= "produce" task-type)
      benchmark-produce-files-by-id
      benchmark-evaluate-files-by-id)
      {:id benchmark-instance-id} db-client))
+
+(defn get-events [db-client task-id]
+  "Fetch all events for a given task"
+  (doall
+    (map #(:body (event/lookup db-client (:id %) {}))
+         (events-by-task-id {:id task-id} db-client))))
 
 (defn show
   "Returns all incomplete tasks"
   [db-client _]
   (->> (incomplete-tasks {} db-client)
        (map :id)
-       (ring/response) ))
+       (ring/response)))
 
 (defn lookup
   "Gets a single task entry by its ID"
   [db-client id _]
-  (let [task  (first (task-by-id {:id id} db-client))
-        files (get-task-files db-client (:benchmark_instance_id task) (:task_type task))]
+  (let [task   (first (task-by-id {:id id} db-client))
+        files  (get-task-files db-client (:benchmark_instance_id task) (:task_type task))
+        events (get-events db-client id)]
     (-> task
         (assoc :inputs files)
+        (assoc :events events)
         (dissoc :benchmark_instance_id)
         (st/rename-keys {:external_id :benchmark, :task_type :type})
         (create-submap image-keys :image)
