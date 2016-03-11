293a206e303a1f7174274084f3160379d3938329
Michael Barton <mail@michaelbarton.me.uk>
2016-03-10 17:16:47 -0800

    Implement test for invalid file types

--- src/nucleotides/api/events.clj
+++ src/nucleotides/api/events.clj
@@ -1,8 +1,11 @@
 (ns nucleotides.api.events
   (:require [yesql.core            :refer [defqueries]]
-            [clojure.walk          :as walk]
-            [clojure.string        :as st]
-            [nucleotides.api.util  :as util]))
+
+            [clojure.walk             :as walk]
+            [clojure.string           :as st]
+            [nucleotides.api.metrics  :as metrics]
+            [nucleotides.api.files    :as files]
+            [nucleotides.api.util     :as util]))
 
 (defqueries "nucleotides/api/events.sql")
 (defqueries "nucleotides/api/metrics.sql")
@@ -59,3 +62,15 @@
 
 (def exists?
   (util/integer-id-exists-fn? get-event))
+
+(defn event-validation-errors [event]
+  {"metrics"    (->> (:metrics event)
+                     (keys)
+                     (metrics/invalid-metrics))
+
+  "file types"  (->> (:files event)
+                      (map :type)
+                      (files/invalid-files))})
+
+(defn valid? [event]
+  (every? empty? (vals (event-validation-errors event))))
