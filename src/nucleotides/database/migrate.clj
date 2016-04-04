431e9089e52778435f4fccd4e3153c1663b10e15
Michael Barton <mail@michaelbarton.me.uk>
2016-04-04 11:23:03 -0700

    Implement loading of metadata in migrate function

--- src/nucleotides/database/migrate.clj
+++ src/nucleotides/database/migrate.clj
@@ -2,21 +2,20 @@
   (:gen-class)
   (:require
     [migratus.core                   :as mg]
-    [nucleotides.database.load       :as loader]
+    [nucleotides.database.metadata   :as mtd]
     [nucleotides.database.files      :as files]
     [nucleotides.database.connection :as con]))
 
-(defn create-migratus-spec [sql-params]
+(defn create-migratus-spec []
   {:store                :database
    :migration-dir        "migrations/"
    :migration-table-name "db_version"
-   :db                   sql-params})
+   :db                   (con/create-connection)})
 
 (defn migrate [directory]
-  (let [data  (files/load-data-files directory)
-        con   (con/create-connection)]
-    (mg/migrate (create-migratus-spec con))
-    (loader/load-data con data)))
+  (let [data (files/load-data-files directory)]
+    (mg/migrate (create-migratus-spec))
+    (mtd/load-all-metadata (:cv data))))
 
 (defn -main [& args]
   (migrate (first args))
--- test/nucleotides/database/migrate_test.clj
+++ test/nucleotides/database/migrate_test.clj
@@ -5,34 +5,18 @@
 
             [nucleotides.database.migrate  :as migrate]))
 
-(use-fixtures :each (fn [f] (drop-tables) (f)))
-
 (def tables
   [:platform-type
    :protocol-type
-   :product-type
    :source-type
    :file-type
    :metric-type
-   :run-mode-type
-
-   :input-data-source
-   :input-data-source-reference-file
-   :input-data-file-set
-   :input-data-file
-
-   :image-type
-   :image-instance
-   :image-instance-task
-
-   :benchmark-type
-   :benchmark-data
-   :benchmark-instance
-   :task
-   :task_expanded_fields])
+   :run-mode-type])
 
 (deftest migrate
+
   (testing "-main"
-    (migrate/migrate (test-directory :data))
-    (dorun (for [table-name tables]
-             (is (not (= 0 (table-length table-name))) (str table-name))))))
+    (do (drop-tables)
+        (migrate/migrate "tmp/input_data")
+        (dorun (for [table-name tables]
+                 (is (not (= 0 (table-length table-name))) (str table-name)))))))
