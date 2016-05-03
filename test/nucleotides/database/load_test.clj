059c6fb36c54944da2e492d30949614dd15e62a5
Michael Barton <mail@michaelbarton.me.uk>
2016-05-02 17:11:20 -0700

    Remove uneeded library from tests

--- test/nucleotides/database/load_test.clj
+++ test/nucleotides/database/load_test.clj
@@ -2,7 +2,6 @@
   (:require [clojure.test     :refer :all]
             [helper.database  :refer :all]
             [helper.fixture   :refer :all]
-    [com.rpl.specter          :refer :all]
 
             [nucleotides.database.files      :as files]
             [nucleotides.database.load       :as ld]
