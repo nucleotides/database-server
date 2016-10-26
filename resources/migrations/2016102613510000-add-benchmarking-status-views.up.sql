2f1924eedf6216267aaa627057dda80c49019cd4
Michael Barton <mail@michaelbarton.me.uk>
2016-10-26 15:52:14 -0700

    Add view of current benchmarking task status

---
+++ resources/migrations/2016102613510000-add-benchmarking-status-views.up.sql
@@ -0,0 +1,18 @@
+--;;
+--;; Status view of the benchmarking database
+--;;
+CREATE VIEW benchmark_task_status AS
+SELECT
+COALESCE(task_type::text, 'all')                                                                            AS task_type,
+COUNT(task_id)                                                                                              AS n,
+COUNT(task_id) filter (where event.event_id IS NULL)                                                        AS n_outstanding,
+COUNT(task_id) filter (where event.event_id IS NOT NULL)                                                    AS n_completed,
+COUNT(task_id) filter (where event.success = true)                                                          AS n_successful,
+COUNT(task_id) filter (where event.success = false)                                                         AS n_errorful,
+round((COUNT(task_id) filter (where event.event_id IS NULL) / COUNT(task_id)::float)::numeric, 4) * 100     AS percent_outstanding,
+round((COUNT(task_id) filter (where event.event_id IS NOT NULL) / COUNT(task_id)::float)::numeric, 4) * 100 AS percent_completed,
+round((COUNT(task_id) filter (where event.success = true) / COUNT(task_id)::float)::numeric, 4) * 100       AS percent_successful,
+round((COUNT(task_id) filter (where event.success = false) / COUNT(task_id)::float)::numeric, 4) * 100      AS percent_errorful
+FROM task
+LEFT JOIN events_prioritised_by_successful                                                                  AS event USING (task_id)
+GROUP BY ROLLUP(task_type);
